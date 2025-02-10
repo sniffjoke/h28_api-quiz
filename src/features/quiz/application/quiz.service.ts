717c0b7e36617ab3c9198f39cfff034d95d4abf5
restitution <sniffjoke@gmail.com>
2025-02-10 15:15:17 +0100

    fix score-record for cron-method

--- src/features/quiz/application/quiz.service.ts
+++ src/features/quiz/application/quiz.service.ts
@@ -35,9 +35,10 @@ export class QuizService {
         }
       }
       if (game.secondPlayerProgress.answers.length === 5) {
-        lastAnswerTime = game.firstPlayerProgress.answers.length ? Date.parse(game.firstPlayerProgress.answers[game.secondPlayerProgress.answers.length - 1].addedAt) : 0;
-        // if (game.secondPlayerProgress.answers.length < 5) {}
-        if (game.firstPlayerProgress.answers.length < 5  && Date.parse(new Date(Date.now()).toString()) - 10000 > Date.parse(game.firstPlayerProgress.answers[game.secondPlayerProgress.answers.length - 1].addedAt)) {
+        // console.log(2);
+        console.log(Date.parse(game.firstPlayerProgress.answers[game.firstPlayerProgress.answers.length - 1].addedAt));
+        lastAnswerTime = game.firstPlayerProgress.answers.length ? Date.parse(game.firstPlayerProgress.answers[game.firstPlayerProgress.answers.length - 1].addedAt) : 0;
+        if (game.firstPlayerProgress.answers.length < 5  && Date.parse(new Date(Date.now()).toString()) - 10000 > Date.parse(game.firstPlayerProgress.answers[game.firstPlayerProgress.answers.length - 1].addedAt)) {
           game = this.quizRepository.calculateScore(game)
           await this.quizRepository.finishGame(game);
         }
