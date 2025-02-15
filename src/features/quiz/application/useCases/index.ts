be8e55878c5d433513549333189bc18ec278ccd2
restitution <sniffjoke@gmail.com>
2025-02-15 01:56:57 +0100

    fix use-cases for questions

--- src/features/quiz/application/useCases/index.ts
+++ src/features/quiz/application/useCases/index.ts
@@ -1,9 +1,11 @@
 import { SendAnswerUseCase } from './send-answer.use-case';
 import { CreateOrConnectUseCase } from './create-or-connect.use-case';
+import { CreateQuestionUseCase } from './create-question.use-case';
 
 export const QuizCommandHandlers = [
   SendAnswerUseCase,
-  CreateOrConnectUseCase
+  CreateOrConnectUseCase,
+  CreateQuestionUseCase
   // DeleteBlogUseCase,
   // UpdatePostWithBlogInParamsUseCase,
   // DeletePostWithBlogInParamsUseCase
